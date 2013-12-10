a555bd58b5dbdf9545e731f3d076a642dc4fed43
REZGUI Mohamed <kyo.alone@gmail.com>
2013-12-10 12:03:39 +0100

    Add json-simple as a json parser in pom.xml
    Instance Parser and tested (need unit test)
    correct json instance

--- src/main/java/notus/JSonParser.java
+++ src/main/java/notus/JSonParser.java
@@ -17,55 +17,229 @@
 package notus;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonWriter;
 
 import parser.absconparseur.tools.UnsupportedConstraintException;
 import parser.instances.InstanceFileParser;
 
 public class JSonParser implements InstanceFileParser {
 
-	public JSonParser() {
-		// TODO Auto-generated constructor stub
-	}
+	File file = null;
+	public static Gson gson = new Gson();
+	Instance instance = null;
+	
+	public static final String ITEMS_KEY = "items";
+	public static final String BINS_KEY = "bins";
+
+	public static final String NAME_KEY = "name";
+	public static final String ARITY_KEY = "arity";
+	public static final String COST_KEY = "cost";
+	public static final String SIZES_KEY = "sizes";
+	
+	public JSonParser() {}
 
 	
 	@Override
 	public File getInstanceFile() {
-		// TODO Auto-generated method stub
-		return null;
+		return file;
 	}
 	
 	public Item[] getItems() {
-		// TODO Auto-generated method stub
-		return null;
+		return instance.items;
 	}
 
 	public Bin[] getBins() {
-		// TODO Auto-generated method stub
-		return null;
+		return instance.bins;
 	}
 
 	public Dimensions getDimensions() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void loadInstance(File file) {
-		// TODO Auto-generated method stub
-
+		this.file = file;
 	}
 
 	@Override
-	public void parse(boolean displayInstance)
-			throws UnsupportedConstraintException {
-		// TODO Auto-generated method stub
+	public void parse(boolean displayInstance) {
+		try {
+			/*
+			instance = gson.fromJson(new FileReader(file),  Instance.class);
+			//{"items":[{"name":"MySQL-LB","consume":[{"ram":128}],"arity":2},{"name":"MySQL-backend","consume":[{"ram":512}],"arity":4},{"name":"Keystone-LB","consume":[{"ram":128}],"arity":2},{"name":"Keystone-backend","consume":[{"ram":128}],"arity":3},{"name":"Glance-API-LB","consume":[{"ram":128}],"arity":2},{"name":"Glance-API-backend","consume":[{"ram":128}],"arity":2},{"name":"Glance-Registry-LB","consume":[{"ram":128}],"arity":2},{"name":"Glance-Registry-backend","consume":[{"ram":128}],"arity":2},{"name":"Glance-DB","consume":[],"arity":1},{"name":"Nova-API-LB","consume":[{"ram":128}],"arity":2},{"name":"Nova-API-backend","consume":[{"ram":128}],"arity":2},{"name":"Nova-Scheduler","consume":[{"ram":128}],"arity":2},{"name":"Nova-Conductor","consume":[{"ram":128}],"arity":2},{"name":"Nova-Compute","consume":[{"ram":1024}],"arity":4},{"name":"Nova-DB","consume":[],"arity":1},{"name":"Queue-LB","consume":[{"ram":128}],"arity":2},{"name":"Queue-backend","consume":[{"ram":128}],"arity":3}],"bins":[{"name":"Location_category_1","provide":[{"ram":2048}],"cost":1,"arity":6}]}
+			System.out.print(gson.toJson(instance, Instance.class));
+			*/
+			Map<String, Map> mapInstance = (Map<String, Map>) JSONUtils.deSerializeData(file);
+			//System.out.print(JSONUtils.serializeData(map));
+			
+			instance = new Instance();
+			
+			List<Map> mapItems = (List<Map>) mapInstance.get(ITEMS_KEY);
+			List<Map> mapBins = (List<Map>) mapInstance.get(BINS_KEY);
+			
+			instance.items = new ItemJSON[mapItems.size()];
+			instance.bins = new BinJSON[mapBins.size()];
+			
+			System.out.println("Get " + ITEMS_KEY);					
+			for(int i = 0; i < instance.items.length; i++) {
+				 ItemJSON item = new ItemJSON();
+				 Map mapItem = mapItems.get(i);
 
+				 item.name = (String) mapItem.get(NAME_KEY);
+				 item.arity = (Long) mapItem.get(ARITY_KEY);
+				 List<Map> mapSizes = (List<Map>) mapItem.get(SIZES_KEY);
+				 
+				 item.sizes = new Size[mapSizes.size()];
+				 
+				 for(int j = 0; j < item.sizes.length; j++) {
+					 Size size = new Size();
+					 size.dims.putAll((Map<String, Integer>) mapSizes.get(j));
+					 item.sizes[j] = size;
+					 //System.out.println(size.dims);
+				 }
+				 
+				 instance.items[i] = item;
+			}
+			
+			System.out.println("Get " + BINS_KEY);					
+			for(int i = 0; i < instance.bins.length; i++) {
+				 BinJSON bin = new BinJSON();
+				 Map mapBin = mapBins.get(i);
+
+				 bin.name = (String) mapBin.get(NAME_KEY);
+				 bin.arity = (Long) mapBin.get(ARITY_KEY);
+				 bin.cost = (Long) mapBin.get(COST_KEY);
+				 
+				 List<Map> mapSizes = (List<Map>) mapBin.get(SIZES_KEY);
+				 
+				 bin.sizes = new Size[mapSizes.size()];
+				 
+				 for(int j = 0; j < bin.sizes.length; j++) {
+					 Size size = new Size();
+					 size.dims.putAll((Map<String, Integer>) mapSizes.get(j));
+					 bin.sizes[j] = size;
+					 //System.out.println(size.dims);
+				 }
+				 
+				 instance.bins[i] = bin;
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void cleanup() {
-		// TODO Auto-generated method stub
+		file = null;	
+		instance = null;	
+	}
+	
+	
+	class Instance {
+	   ItemJSON[] items;
+	   BinJSON[]  bins;
+	}
 
+	
+	class ItemJSON implements Item {
+		
+		//{ "name": "MySQL-LB", "consume": [ {"ram", 128} ], "arity": 2 },
+	    
+		String name;
+		Size[] sizes;
+		long arity;
+		
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public int getSize(int dim) {
+			return sizes[dim].getDimensionCount();
+		}
+		
+		@Override
+		public int getArity() {
+			return (int) arity;
+		}
+	}
+
+	class BinJSON implements Bin {
+		 /*
+		{
+		      "name": "Location_category_1",
+		      "provide": [ {"ram", 2048} ],
+		      "cost": 1,
+		      "arity": 6
+		    }
+		    */
+		String name;
+		Size[] sizes;
+		long cost;
+		long arity;
+
+		@Override
+		public String getName() {
+			return name;
+		}
+		@Override
+		public int getSize(int dim) {
+			return sizes[dim].getDimensionCount();
+		}
+		@Override
+		public int getArity() {
+			return (int) arity;
+		}
+		@Override
+		public int getCost() {
+			// TODO Auto-generated method stub
+			return (int) cost;
+		}	
+	}
+	
+	class Size implements Dimensions {
+		
+		Map<String, Integer> dims = new LinkedHashMap<String, Integer>();
+		
+		public Size() {}
+		
+		@Override
+		public int getDimensionCount() {
+			return dims.size();
+		}
+
+		@Override
+		public int getIndex(String dim) {
+			return dims.get(dim);
+		}
+
+		@Override
+		public String getName(int dim) {
+			//http://stackoverflow.com/questions/3478061/does-javas-linkedhashmap-maintain-the-order-of-keys
+			return (String) dims.keySet().toArray()[dim];
+		}	
+	}
+	
+	public static void main(String[] args) {
+		JSonParser json = new JSonParser();
+		json.loadInstance(new File("src/test/resources/veryredundant-highmysqlandnovacompute-wheezy/binpacking.json"));
+		json.parse(true);
 	}
 
 }
+
